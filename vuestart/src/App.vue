83ae5e3ffdfeb941ac9862dd8dd98a9d352f348d
lilutily <tjdxodnd225@gmail.com>
2023-03-03 13:14:25 +0900

    vue 데이터바인딩

--- vuestart/src/App.vue
+++ vuestart/src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  하이 ㅋ
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- 바뀔일이 없으면 안해도 될듯 logo 같은 경우는 변경이 실시간으로 될 필요가 있을까? -->
+    {{ logo }}
+    <h4 class="red" :style="스타일">XX 원룸</h4>    
+    {{ products[0] }}
+    <p>{{ price1 }} 만원</p>
+  </div>
+
+  <div>
+    <h4 class="red" style="color:rebeccapurple">XX 원룸</h4>
+    {{ products[1] }}
+    <p>{{ price2 }} 만원</p>
+  </div>
+
+  <div>
+    <h4 class="red" style="color:rebeccapurple">XX 원룸</h4>
+    {{ products[2] }}
+    <p>{{ price2 }} 만원</p>
+  </div>
   
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// document.getElementById().innerHTML = ?? 자바스크립트 바인딩
+// 데이터 바인딩 왜쓰는지? 
 
 export default {
   name: 'App',
+  data() {
+    return  { // 중괄호니까 object 자료로 저장
+      // 데이터 보관함
+      price1 : 60,
+      price2 : 70,
+      logo : '원룸샵',
+      스타일 : 'color :blue',
+      products : ['원룸1', '원룸2', '원룸3']
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
